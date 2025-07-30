e43727b65a7c71ecf21e5fe1ac16b198a3237e08
Ananth-Hari-Narain <ananth.harinarain@gmail.com>
2025-07-30 14:42:21 +0100

    Implemented auto generate flashcard answers and auto underline.

--- ai_wrapper.py
+++ ai_wrapper.py
@@ -1,11 +1,24 @@
-def auto_generate_answer(term: str, subject, length) -> str:
+from google import genai
+from os import getenv
+
+def auto_generate_answer(term: str, subject, length="brief", underlineKeyWords=False) -> str:
     """
     Uses an LLM API
     :return: answer to a question or give a definition to a term.
     """
-    pass
+    client = genai.Client(api_key=getenv("GEMINI_API_KEY"))
+    chat = client.chats.create(model="gemini-2.5-flash")
+    prompt = f"Flashcard answer {subject}:{term}[{length}, no #s/links]"
+    response = chat.send_message(prompt).text
+    if underlineKeyWords:
+        keywords = chat.send_message("List relevant keywords, separate by ;").text
+        keywords = keywords.replace("; ", ";")
+        for keyword in keywords.lower().split(";"):
+            response = response.replace(keyword, f"_{keyword}_", -1)
 
-def auto_generate_tags():
+    return response
+
+def auto_generate_key_():
     """
     Uses an NLP to generate tags to determine the topics
     of a particular flashcard set
@@ -19,3 +32,6 @@ def check_answer_is_correct() -> bool:
     given by the flashcards.
     """
     pass
+
+
+print(auto_generate_answer("What is the purpose of the status register?", "CS A-Level", "brief", True))
