bae99e105c65545bc455aa98c1c56ecdd4b93202
Ananth-Hari-Narain <ananth.harinarain@gmail.com>
2025-08-04 10:55:42 +0100

    Implemented answer checking with LLM and used .env file

--- ai_wrapper.py
+++ ai_wrapper.py
@@ -1,5 +1,8 @@
 from google import genai
 from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv("file.env")
 
 def auto_generate_answer(term: str, subject, length="brief", underlineKeyWords=False) -> str:
     """
@@ -11,27 +14,21 @@ def auto_generate_answer(term: str, subject, length="brief", underlineKeyWords=F
     prompt = f"Flashcard answer {subject}:{term}[{length}, no #s/links]"
     response = chat.send_message(prompt).text
     if underlineKeyWords:
-        keywords = chat.send_message("List relevant keywords, separate by ;").text
+        keywords = chat.send_message("List only most relevant keywords, separate by ;").text
         keywords = keywords.replace("; ", ";")
         for keyword in keywords.lower().split(";"):
             response = response.replace(keyword, f"_{keyword}_", -1)
 
     return response
 
-def auto_generate_key_():
+def check_answer_is_correct(user_answer, actual_answer) -> bool:
     """
-    Uses an NLP to generate tags to determine the topics
-    of a particular flashcard set
-    :return:
-    """
-    pass
-
-def check_answer_is_correct() -> bool:
-    """
-    Uses an NLP API to determine if an answer given by a user matches the answer
+    Uses an LLM API to determine if an answer given by a user matches the answer
     given by the flashcards.
     """
-    pass
-
-
-print(auto_generate_answer("What is the purpose of the status register?", "CS A-Level", "brief", True))
+    client = genai.Client(api_key=getenv("GEMINI_API_KEY"))
+    chat = client.chats.create(model="gemini-2.5-flash")
+    prompt = f'yes or no: does "{user_answer}" match the real answer: "{actual_answer}"'
+    response = chat.send_message(prompt).text
+    response.removesuffix(".")
+    return response.lower() == "yes"
